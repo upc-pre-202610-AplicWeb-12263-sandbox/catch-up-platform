aac55bbc0961d0fb177c3d472a7e9bf765c91e66
Alumnos <alumnos@A509-SERV.local>
2026-05-28 20:59:47 -0500

    chore: edit and add files in directories Properties and Resources

---
+++ CatchUpPlatform.API/Resources/SharedResource.cs
@@ -0,0 +1,10 @@
+namespace CatchUpPlatform.API.Resources;
+
+/// <summary>
+///     Marker class used by ASP.NET Core's <c>IStringLocalizer</c> to locate the shared
+///     resource files (<c>SharedResource.*.resx</c>) for localized response messages.
+/// </summary>
+// ReSharper disable once ClassNeverInstantiated.Global
+public class SharedResource
+{
+}
